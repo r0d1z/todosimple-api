993794c40b6da8acea03dbaf9e93d4894be9a350
r0d1z <rodrigo.guimares@icomp.ufam.edu.br>
2024-03-05 02:43:04 -0400

    Create task controller

--- src/main/java/com/rodinh0/todosimple/services/TaskService.java
+++ src/main/java/com/rodinh0/todosimple/services/TaskService.java
@@ -1,5 +1,6 @@
 package com.rodinh0.todosimple.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,11 @@ public class TaskService {
         return task.orElseThrow(() -> new RuntimeException(
             "Tarefa nao encotrada!"
         ));
+    }
+
+    public List<Task> findAllByUserId(Long userId){
+        List<Task> tasks = this.taskRepository.findByUser_Id(userId);
+        return tasks;
     }
 
     @Transactional

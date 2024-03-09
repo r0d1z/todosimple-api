a2e596ac9625008a666a76e8c16022b05332f989
r0d1z <rodrigo.guimares@icomp.ufam.edu.br>
2024-03-09 17:26:54 -0400

    implement web config

--- src/main/java/com/rodinh0/todosimple/config/WebConfig.java
+++ src/main/java/com/rodinh0/todosimple/config/WebConfig.java
@@ -1,7 +1,15 @@
 package com.rodinh0.todosimple.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer{
-    
+
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**");
+    }    
 }

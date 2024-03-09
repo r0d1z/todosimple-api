c4cf35fc07007fdf8c75c6d79da81e9859035c92
r0d1z <rodrigo.guimares@icomp.ufam.edu.br>
2024-03-09 17:04:49 -0400

    Refactor task controller

--- src/main/java/com/rodinh0/todosimple/controllers/TaskController.java
+++ src/main/java/com/rodinh0/todosimple/controllers/TaskController.java
@@ -18,6 +18,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rodinh0.todosimple.models.Task;
 import com.rodinh0.todosimple.services.TaskService;
+import com.rodinh0.todosimple.services.UserService;
 
 import jakarta.validation.Valid;
 
@@ -29,6 +30,9 @@ public class TaskController {
     @Autowired
     private TaskService taskService;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/{id}")
     public ResponseEntity<Task> findById(@PathVariable Long id){
 
@@ -39,6 +43,7 @@ public class TaskController {
     
     @GetMapping("/user/{userId}")
     public ResponseEntity<List<Task>> findAllByUserId(@PathVariable Long userId){
+        this.userService.findById(userId);
         List<Task> tasks = this.taskService.findAllByUserId(userId);
 
         return ResponseEntity.ok().body(tasks);
